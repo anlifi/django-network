6adb96442f32ce1e537b6c9a1aeaff2462357b51
Fischer, Angelina <angelina.fischer@sva.de>
2023-06-01 17:30:21 +0200

    Add models for posts, likes and followers

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Post, Like, Follower
 
 # Register your models here.
+admin.register(User)
+admin.register(Post)
+admin.register(Like)
+admin.register(Follower)
--- network/models.py
+++ network/models.py
@@ -4,3 +4,38 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+    def __str__(self):
+        return f"User ID: {self.pk}, Username: {self.username}"
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    content = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} created post {self.pk} on {self.create_date}, last updated: {self.update_date}"
+    
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post {self.post.pk} liked by {self.user.username}"
+    
+
+class Follower(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name="followers")
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} started following {self.following}"
+    
+    def is_valid_follower(self):
+        # User should not be able to follow himself
+        return self.user != self.following
